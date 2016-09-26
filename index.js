81be99279542577814ea0b5338659f02a787cafd
fisker <lionkay@gmail.com>
2016-09-27 02:37:38 +0800

    update

--- index.js
+++ index.js
@@ -5,42 +5,19 @@
 
 'use strict';
 
-var path = require('path');
-var nodePath = 'node';
-var spawnSync = require('child_process').spawnSync;
-var stylefmtBin = path.normalize(path.join(__dirname, './bin/stylefmt.js'));
+var syncPromise = require('promise-synchronizer');
 var log = (global.fis && fis.log) || console;
-function nodeBin() {
-  var args = [].slice.call(arguments).map(function(arg) {
-    return typeof arg === 'string' ? arg : JSON.stringify(arg);
-  });
-  var result = spawnSync(nodePath, args, {encoding: 'utf-8'});
-
-  if (result.error) {
-    log.warn(
-      result.error.message,
-      result.error.stack
-    );
-    process.exit(1);
-  }
-
-  result = result.stdout + '';
-
-  try {
-    result = JSON.parse(result);
-  } catch(err) {
-    result = null;
-  }
-
-  return result;
-}
 
 module.exports = function(content, file, conf){
-  var result = nodeBin(stylefmtBin, content);
-  if (result && result.css) {
-    return result.css;
-  }
 
+  var promise = stylefmt(content, {})
+    .then(function(result) {
+      if (result && result.css) {
+        content = result.css;
+      }
+    });
+
+  syncPromise(promise);
   return content;
 };
 
