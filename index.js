ee6df6d582374354747d75aefe0caceaa008ff7e
fisker <lionkay@gmail.com>
2017-02-09 15:52:07 +0800

    fix code style

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 
 require('es6-shim');
 var syncPromise = require('promise-synchronizer');
-var stylefmt = require('stylefmt').process;;
+var stylefmt = require('stylefmt').process;
 var log = (global.fis && fis.log) || console;
 
 module.exports = function(content, file, conf){
