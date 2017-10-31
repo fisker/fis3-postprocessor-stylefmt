6ff3ccb4e64788ac8c97dcebfa2e3a75223bf0b3
fisker <lionkay@gmail.com>
2017-10-31 20:54:45 +0800

    bug fix

--- index.js
+++ index.js
@@ -3,31 +3,31 @@
  * fisker Cheung<lionkay@gmail.com>
  */
 
-'use strict';
+'use strict'
 
 // colors.js bug
 // es6-shim `bold` breaks colors.js
-var getter = String.prototype.__lookupGetter__('bold');
-require('es6-shim');
+var getter = String.prototype.__lookupGetter__('bold')
+require('es6-shim')
 if (getter) {
   String.prototype.__defineGetter__('bold', function() {
-    return this;
-  });
+    return this
+  })
 }
 
-var syncPromise = require('promise-synchronizer');
-var stylefmt = require('stylefmt').process;
-var log = (global.fis && fis.log) || console;
+var syncPromise = require('promise-synchronizer')
+var stylefmt = require('stylefmt').process
+var log = (global.fis && fis.log) || console
 
-module.exports = function(content, file, conf){
-
-  var promise = stylefmt(content, {})
-    .then(function(result) {
-      if (result && result.css) {
-        content = result.css;
-      }
-    });
-
-  syncPromise(promise);
-  return content;
-};
+module.exports = function(content, file, conf) {
+  try {
+    content = syncPromise(
+      stylefmt(content, {}).then(function(result) {
+        if (result && result.css) {
+          content = result.css
+        }
+      })
+    )
+  } catch (err) {}
+  return content
+}
